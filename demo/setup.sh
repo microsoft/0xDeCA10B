ff77c01aadce3e30c556be51492186e7d3999794
Justin Harris <justindharris@gmail.com>
2019-07-12 18:10:55 -0400

    demo/setup: Add setup libs script.

--- demo/setup.sh
+++ demo/setup.sh
@@ -12,14 +12,4 @@ fi
 yarn install
 (cd client && yarn install)
 
-safe_math_file="client/contracts/libs/SafeMath.sol"
-mkdir --parents `dirname "${safe_math_file}"`
-wget https://github.com/OpenZeppelin/openzeppelin-solidity/raw/1fd993bc01890bf6bd974aaf3d709bdf0a79b9bf/contracts/math/SafeMath.sol --output-document "${safe_math_file}"
-# Change the first line to use the right compiler version.
-sed -i "1s/.*/pragma solidity ^0.5;/" "${safe_math_file}"
-
-safe_math_file="client/contracts/libs/SignedSafeMath.sol"
-mkdir --parents `dirname "${safe_math_file}"`
-wget https://github.com/OpenZeppelin/openzeppelin-solidity/raw/1fd993bc01890bf6bd974aaf3d709bdf0a79b9bf/contracts/drafts/SignedSafeMath.sol --output-document "${safe_math_file}"
-# Change the first line to use the right compiler version.
-sed -i "1s/.*/pragma solidity ^0.5;/" "${safe_math_file}"
+./setup_libs.sh
