f8d284785fcc84f0429ea536bacbbe0e0547b627
Justin Harris <justin.harris@microsoft.com>
2019-08-20 16:37:36 -0400

    demo/DensePerceptron: Add tests.

--- demo/client/test/contracts/classification/denseperceptron.js
+++ demo/client/test/contracts/classification/denseperceptron.js
@@ -4,11 +4,21 @@ contract('DensePerceptron', function (accounts) {
   const toFloat = 1E9;
 
   const classifications = ["NEGATIVE", "POSITIVE"];
-  const weights = convertData([0, 5, -1]);
+  const weights = convertData([0, 1, -1]);
   const intercept = web3.utils.toBN(0 * toFloat);
   const learningRate = 1;
 
   let classifier;
+
+  function convertData(data) {
+    return data.map(x => Math.round(x * toFloat)).map(web3.utils.toBN);
+  }
+
+  async function normalize(data) {
+    data = convertData(data);
+    const norm = await classifier.norm(data);
+    return data.map(x => x.mul(web3.utils.toBN(toFloat)).div(norm));
+  }
 
   function convertNum(num) {
     return web3.utils.toBN(Math.round(num * toFloat));
@@ -23,19 +33,36 @@ contract('DensePerceptron', function (accounts) {
     }
   }
 
+  async function predict(data) {
+    data = await normalize(data);
+    return parseBN(await classifier.predict(data));
+  }
+
   before("deploy classifier", async () => {
     classifier = await Classifier.new(classifications, weights, intercept, learningRate);
   });
 
   it("...should predict the classification POSITIVE", async () => {
-    // TODO
+    const prediction = await predict([0, 2, 1]);
+    assert.equal(prediction, 1, "Wrong classification.");
   });
 
   it("...should predict the classification NEGATIVE", async () => {
-    // TODO
+
+    const prediction = await predict([1, 0, 2])
+    assert.equal(prediction, 0, "Wrong classification.");
   });
 
   it("...should update", async () => {
-    // TODO
+    const data = [1, 1, 1];
+    const classification = await predict(data);
+    const updateResponse = await classifier.update(await normalize(data), classification);
+    assert.isBelow(updateResponse.receipt.gasUsed, 2E5, "Too much gas used.");
+    // console.log(`  update gasUsed: ${updateResponse.receipt.gasUsed}`);
+    assert.equal(await predict(data), classification);
+
+    const newClassification = 1 - classification;
+    await classifier.update(await normalize(data), newClassification);
+    assert.equal(await predict(data), newClassification);
   });
 });
