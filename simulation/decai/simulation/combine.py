122d21f3b213e96beae3ebe3302acdb1db05a5c3
Justin Harris <justin.harris@microsoft.com>
2020-01-10 17:10:55 -0500

    simulation: Start combiner.

---
+++ simulation/decai/simulation/combine.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class SimulationCombiner(object):
+    def combine(self, paths: List[str]):
+        """
+        Combine runs from several files.
+
+        :param paths: The paths to the runs to combine.
+        """
+        pass
