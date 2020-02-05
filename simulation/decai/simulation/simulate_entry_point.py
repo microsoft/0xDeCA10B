972ecdf588c8082603342ebd6bf054dab2e5b9d2
Justin Harris <justindharris@gmail.com>
2020-02-05 14:37:40 -0500

    simulation/news: Update baselines since data is integers now.

--- simulation/decai/simulation/simulate_entry_point.py
+++ simulation/decai/simulation/simulate_entry_point.py
@@ -53,7 +53,7 @@ def main():
                     # train_size, test_size = 3500, 1500
                     fitness=0.97,
                     # train_size, test_size = None, None
-                    news=0.8615,
+                    news=0.8528,
                 )),
         ncc=dict(module=NearestCentroidClassifierModule,
                  baseline_accuracy=dict(
@@ -62,7 +62,7 @@ def main():
                      # train_size, test_size = 3500, 1500
                      fitness=0.9513,
                      # train_size, test_size = None, None
-                     news=0.8324,
+                     news=0.6751,
                  )),
     )
 
