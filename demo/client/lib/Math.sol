3e1dfe19fd045ca77d3b7e33420fb316295e5786
Justin D. Harris <juharris@users.noreply.github.com>
2022-10-20 16:42:07 -0400

    Math.sol: Correct docs.

--- demo/client/lib/Math.sol
+++ demo/client/lib/Math.sol
@@ -8,8 +8,8 @@ library Math {
     /**
      * @return The square root of `x` using the Babylonian method.
      */
-    // Copied from https://github.com/ethereum/dapp-bin/pull/50.
-    function sqrt(uint x) internal pure returns (uint y) {
+    // Copied from https://github.com/ethereum/dapp-bin/pull/50 and modified slightly.
+    function sqrt(uint x) internal pure returns (uint) {
         if (x == 0) return 0;
         else if (x <= 3) return 1;
         uint z = (x + 1) / 2;
